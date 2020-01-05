d9c3349e02e4e0bec11274edb1bcef2c2ca0e610
Nischal Shakya <nischal.shk@gmail.com>
2020-01-05 23:24:45 +0545

    Routing and db correction

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
-//const taskRouter = require('./routes/tasks');
-//const categoryRouter = require('./routes/category');
+
 const userRouter = require('./route/users');
 const dotenv = require('dotenv').config();
-//const uploadRouter = require('./routes/upload');
+const uploadRouter = require('./route/upload');
 const auth = require('./auth');
 const cors = require('cors');
 
 
 const app=express();
+
+app.use(morgan('tiny'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.options('*', cors());
+app.use(express.urlencoded({extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));
 
 
-app.use('/users');
+app.use('/users',userRouter);
+app.use('/upload', uploadRouter);
+app.use(auth.verifyUser);
 
 app.use((err,req,res,next)=>{
     console.error(err.stack);
